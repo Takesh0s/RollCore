b0a7a1d0ddad6a6793281ac6bfb329db83e8da17
Takesh0s <luizphillipedf85@gmail.com>
2026-04-11 19:42:31 -0300

    chore(capacitor): initialize Capacitor configuration and install core dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'rpg.rollcore.app',
+  appName: 'rollcore',
+  webDir: 'dist'
+};
+
+export default config;
